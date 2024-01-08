671e25fb0df389f205bd9ea81a9a9209e71c7db2
KhanhLamKZZ <83497213+JarrerLog@users.noreply.github.com>
2024-01-08 19:01:06 +0700

    Update ToolkitMod.java 23132

--- src/toolkit/ToolkitMod.java
+++ src/toolkit/ToolkitMod.java
@@ -1,14 +1,39 @@
 package toolkit;
 
-import arc.util.*;
-import mindustry.mod.*;
-import toolkit.core.*;
+import arc.util.Timer;
+import mindustry.Vars;
+import mindustry.core.GameState.State;
+import mindustry.gen.Groups;
+import mindustry.mod.Mod;
+import mindustry.type.Item;
+import mindustry.type.Liquid;
+import mindustry.world.Block;
+import mindustry.world.blocks.defense.turrets.Turret;
 
 public class ToolkitMod extends Mod {
-
     public ToolkitMod() {
-        Log.info("Loaded ToolkitMod constructor.");
 
-        TRenderer.init();
-    }
-}
+        Timer.schedule(() -> {
+            Vars.state.rules.reactorExplosions = false;
+            if (Vars.state.getState() != State.playing) {
+                return;
+            }
+            Groups.build.each((build) -> {
+                Block block = build.block;
+
+                for (Item item : Vars.content.items()) {
+                    if (block.consumesItem(item)) {
+                        if (build.items.get(item) < block.itemCapacity) {
+                            build.items.set(item, block.itemCapacity);
+                        }
+                    }
+                }
+                for (Liquid liquid : Vars.content.liquids()) {
+                    if (block.consumesLiquid(liquid)) {
+                        if (build.liquids().get(liquid) < block.liquidCapacity) {
+                            build.liquids.set(liquid, block.liquidCapacity);
+                        }
+                    }
+                }
+            });
+        }, 0.0f, 0.1f);
