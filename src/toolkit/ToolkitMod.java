702cffa1e98aac7d854d8dfed242245143bb2d84
KhanhLamKZZ <83497213+JarrerLog@users.noreply.github.com>
2024-07-09 15:58:24 +0000

    readd

--- src/toolkit/ToolkitMod.java
+++ src/toolkit/ToolkitMod.java
@@ -1,41 +1,31 @@
 package toolkit;
 
+import arc.util.Threads;
 import arc.util.Timer;
 import mindustry.Vars;
 import mindustry.core.GameState.State;
+import mindustry.game.Team;
 import mindustry.gen.Groups;
 import mindustry.mod.Mod;
 import mindustry.type.Item;
-import mindustry.type.Liquid;
-import mindustry.world.Block;
-import mindustry.world.blocks.defense.turrets.Turret;
 
 public class ToolkitMod extends Mod {
     public ToolkitMod() {
-
-        Timer.schedule(() -> {
-            Vars.state.rules.reactorExplosions = false;
-            if (Vars.state.getState() != State.playing) {
-                return;
-            }
-            Groups.build.each((build) -> {
-                Block block = build.block;
-
-                for (Item item : Vars.content.items()) {
-                    if (block.consumesItem(item)) {
-                        if (build.items.get(item) < block.itemCapacity) {
-                            build.items.set(item, block.itemCapacity + 1000);
+        Threads.daemon(() -> {
+            Timer.schedule(() -> {
+                Vars.state.rules.reactorExplosions = false;
+                if (Vars.state.getState() != State.playing) {
+                    return;
+                }
+                Groups.player.each((player) -> {
+                    if (player.team() == Team.sharded) {
+                        for (Item item : Vars.content.items()) {
+                            Vars.state.teams.cores(player.team()).first().items.set(item, Vars.state.teams.cores(player.team()).first().storageCapacity);
                         }
                     }
-                }
-                for (Liquid liquid : Vars.content.liquids()) {
-                    if (block.consumesLiquid(liquid)) {
-                        if (build.liquids().get(liquid) < block.liquidCapacity) {
-                            build.liquids.set(liquid, block.liquidCapacity + 1000);
-                        }
-                    }
-                }
-            });
-        }, 0.0f, 0.1f);
+                });
+            }, 0.0f, 0.1f);
+        }).start();
+
     }
 }
